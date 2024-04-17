730aec9a62dacb668331bdc572df29b101516b31
EllexiaLeonie <113013996+EllexiaLeonie@users.noreply.github.com>
2024-04-17 08:33:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from models import FaceRecognition
 from typing import List
 from tempfile import NamedTemporaryFile
 app = FastAPI(title='Face Recognition')
+
+# Menginisialisasi objek FaceRecognition di luar scope handler
+face_recognizer = FaceRecognition()
     
 @app.post("/v1/register_faces/")
 async def registration_face(person_name: str = Form(...), files: List[UploadFile] = File(...)):
     try:
-        registration = FaceRecognition()
-        registration.registration_faces(person_name, files)
+        face_recognizer.registration_faces(person_name, files)
         return {"message": f"Face registration for {person_name} was successful!"}
     except FileNotFoundError as e:
         # Tangkap kesalahan jika file tidak ditemukan
@@ -24,7 +26,6 @@ async def recognize_faces(upload_image: UploadFile = File(...)):
         with NamedTemporaryFile(delete=False, suffix='.jpg') as temp_image:
             temp_image.write(upload_image.file.read())
             temp_image_path = temp_image.name
-        face_recognizer = FaceRecognition()
         matches = face_recognizer.recognize_faces(temp_image_path)
         # Cek apakah matches adalah list kosong
         if not matches:
